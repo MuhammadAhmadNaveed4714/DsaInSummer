1c467fc499dc8ae1af2e7a87753741911160e87a
Maze_Runner-Git <mynameisahmad4714@gmail.com>
2025-08-03 23:45:03 +0500

    Commit Eighteen

---
+++ BSEF23A020-Assignment2task1.cpp
@@ -0,0 +1,44 @@
+//#include<iostream>
+//using namespace std;
+//// Function odd
+//int even(int* a, int s, int i)
+//{
+//	if (i >= s)
+//	{
+//		return 0;
+//	}
+//	if (i < s)
+//	{
+//		i = i + 2;
+//		return a[i - 2] + even(a, s, i);
+//	}
+//}
+//// Function odd
+//int odd(int* a, int s, int i)
+//{
+//	if (i >= s)
+//	{
+//		return 0;
+//	}
+//	if (i < s)
+//	{
+//		i = i + 2;
+//		return a[i - 2] + odd(a, s, i);
+//	}
+//}
+//int main()
+//{
+//	int size = 0;
+//		cout << "\n\t\t\t\t\tEnter size of array : ";
+//		cin >> size;
+//		int* q = new int[size];
+//		for (int i = 0; i < size; i++)
+//		{
+//			cout << "\n\t\t\t\t\tEnter element [" << i << "] = ";
+//			cin >> q[i];
+//		}
+//		cout << "\n\t\t\t\t\tGreat Robbery : " << ((even(q,size,0)>odd(q,size,1))?even(q,size,0):odd(q,size,1)) << "\n";
+//		delete[]q;
+//		q = nullptr;
+//	return 0;
+//}
