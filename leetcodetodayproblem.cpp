066b3138af3d149373d5303370fb330c64d48232
Maze_Runner-Git <mynameisahmad4714@gmail.com>
2025-08-09 23:18:52 +0500

    Commit twenty5

---
+++ leetcodetodayproblem.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//bool ispoweroftwo(int n)
+//{
+//    if (n <= 0)
+//    {
+//        return false;
+//    }
+//    for (int i = 0; i*i <= n; i++) 
+//    {
+//        if (pow(2,i) == n)
+//        {
+//            return true;
+//        }
+//    }
+//    return false;
+//}
+//int main() 
+//{
+//    int n;
+//    cout << "\n\t\t\t\t\tEnter number: ";
+//    cin >> n;
+//    if (ispoweroftwo(n))
+//    {
+//        cout << "\n\t\t\t\t\tThe given number is a power of two.\n";
+//    }
+//    else
+//    {
+//        cout << "\n\t\t\t\t\tThe given number is not a power of two.\n";
+//    }
+//    return 0;
+//}
