57636adaf59cf7567e7f4e1df5505811d9b2e386
Muhammad Ahmad Naveed <mynameisahmad4714@gmail.com>
2026-06-10 18:13:14 +0500

    Add files via upload

---
+++ linkedlist4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node(int new_data) {
+        data = new_data;
+        next = nullptr;
+    }
+};
+
+
+
+void printList(Node *node) {
+    while (node != nullptr) {
+        cout << node->data;
+        if(node->next != NULL){
+            cout << " -> ";
+        }
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main() {
+
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+
+    printList(head);
+
+    return 0;
+}
